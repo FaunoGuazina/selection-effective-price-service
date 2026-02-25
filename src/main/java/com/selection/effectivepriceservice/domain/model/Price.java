e5da8c7b87f741cdeafc9da65c7b803ed2f07d54
Fauno Guazina <prof.guazina@gmail.com>
2026-02-25 07:32:24 +0100

    feat(domain): model price aggregate

---
+++ src/main/java/com/selection/effectivepriceservice/domain/model/Price.java
@@ -0,0 +1,14 @@
+package com.selection.effectivepriceservice.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record Price(
+    Long brandId,
+    Long productId,
+    Integer priceList,
+    Integer priority,
+    LocalDateTime startDate,
+    LocalDateTime endDate,
+    BigDecimal price,
+    String currency) {}
